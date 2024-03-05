9a5003ade8dd11859ffdbb5941f269e9100cc880
Narendra2281 <narendrakachare7@gmail.com>
2024-03-05 14:33:23 +0530

    Delete files

--- Angular Custom Directive/app/marvellous.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { MarvellousDirective } from './marvellous.directive';
-
-describe('MarvellousDirective', () => {
-  it('should create an instance', () => {
-    const directive = new MarvellousDirective();
-    expect(directive).toBeTruthy();
-  });
-});
