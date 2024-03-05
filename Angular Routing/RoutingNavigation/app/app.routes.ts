a35b0eaf80eaafcf7963217db6e26e7da349cc5b
Narendra2281 <narendrakachare7@gmail.com>
2024-03-05 14:30:34 +0530

    Delete Files From Main

--- Angular Routing/RoutingNavigation/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import { BatchdetailsComponent } from './batchdetails/batchdetails.component';
-import { BatchlistComponent } from './batchlist/batchlist.component';
-
-export const routes: Routes = [
-    {path: 'batchlist', component: BatchlistComponent},
-    {path: 'batchdetails', component: BatchdetailsComponent},
-];
